a8cd3cb5fef6be66fc1543d0d9e60dd125092447
kulagina <olga.kulagina17@yandex.ru>
2022-05-15 18:17:30 +0300

    add authMiddleware

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express, {Request, Response} from 'express'
 import cors from 'cors'
 import {bloggersRouter} from "./routes/bloggersRouter";
 import {postsRouter} from "./routes/postsRouter";
+import {authMiddleware} from "./middlewares/authMiddleware";
 
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+app.use(authMiddleware)
 
 app.use('/bloggers', bloggersRouter)
 app.use('/posts', postsRouter)
---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,12 @@
+import {Request, Response, NextFunction} from "express";
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    if (req.method === 'GET') {
+        next()
+    }
+    if (req.headers.authorization === 'Basic YWRtaW46cXdlcnR5') {
+        next()
+    } else {
+        res.send(401)
+    }
+}
