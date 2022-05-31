a50540e3b6b2eb9a4a697fb3445dfd83714fe3ae
kulagina <olga.kulagina17@yandex.ru>
2022-05-31 18:22:08 +0300

    fixed users pagination

--- src/repositories/usersRepository.ts
+++ src/repositories/usersRepository.ts
@@ -21,8 +21,14 @@ export type GetUserType = {
 
 export const usersRepository = {
     async findUsers(PageNumber?: string | null | undefined , PageSize?: string | null | undefined): Promise<GetUserType> {
-        let a = PageNumber || 1
-        let b = PageSize || 10
+        let a = 1
+        if (PageNumber) {
+            let b = +PageNumber !== null ? +PageNumber : 1
+        }
+        let b = 10
+        if (PageSize) {
+            let b = +PageSize !== null ? +PageSize : 10
+        }
         let users = await usersCollection.find({}, {projection: {_id: 0}}).toArray()
         let items = await usersCollection.find({}, {projection: {_id: 0}}).skip((+a - 1) * +b).limit(+b).toArray()
 
