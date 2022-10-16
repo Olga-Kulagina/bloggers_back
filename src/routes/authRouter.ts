c802e97f8685448dace324c275589094b7174826
kulagina <olga.kulagina17@yandex.ru>
2022-10-16 23:44:38 +0300

    fixed authorization

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -56,34 +56,38 @@ authRouter.get('/me', authBearerMiddleware,
 
 authRouter.post('/refresh-token',
     async (req: Request, res: Response) => {
-        const refreshToken = req.cookies.refreshToken
-        const expiredTime = await jwtUtility.getExpiredTimeForRefresh(refreshToken)
-        if (!expiredTime || expiredTime && Date.now() / 1000 > +expiredTime) {
+        if (!req.headers || !req.body) {
             res.send(401)
         } else {
-            const userId = await jwtUtility.getUserFromRefreshJWT(refreshToken)
-            let user = {
-                id: userId as string
-            }
-            const isValid = await tokensRepository.isValidRefreshToken(user.id, refreshToken)
-            if (!isValid) {
+            const refreshToken = req.cookies.refreshToken
+            const expiredTime = await jwtUtility.getExpiredTimeForRefresh(refreshToken)
+            if (!expiredTime || expiredTime && Date.now() / 1000 > +expiredTime) {
                 res.send(401)
             } else {
-                if (user) {
-                    const token = await jwtUtility.createJWT(user.id)
-                    const newRefreshToken = await jwtUtility.createRefreshJWT(user.id)
-                    res.cookie('refreshToken', newRefreshToken, {
-                        httpOnly: true,
-                        secure: true
-                    })
-                    await tokensRepository.updateTokens({
-                        userId: user.id,
-                        accessToken: token,
-                        refreshToken: refreshToken
-                    })
-                    res.status(200).send({accessToken: token})
+                const userId = await jwtUtility.getUserFromRefreshJWT(refreshToken)
+                let user = {
+                    id: userId as string
+                }
+                const isValid = await tokensRepository.isValidRefreshToken(user.id, refreshToken)
+                if (!isValid) {
+                    res.send(401)
                 } else {
-                    res.sendStatus(401)
+                    if (user) {
+                        const token = await jwtUtility.createJWT(user.id)
+                        const newRefreshToken = await jwtUtility.createRefreshJWT(user.id)
+                        res.cookie('refreshToken', newRefreshToken, {
+                            httpOnly: true,
+                            secure: true
+                        })
+                        await tokensRepository.updateTokens({
+                            userId: user.id,
+                            accessToken: token,
+                            refreshToken: refreshToken
+                        })
+                        res.status(200).send({accessToken: token})
+                    } else {
+                        res.sendStatus(401)
+                    }
                 }
             }
         }
