b14c4f295a1c4aa3f0f4c2cbff4dc478f91d9d96
kulagina <olga.kulagina17@yandex.ru>
2022-10-16 23:03:08 +0300

    fixed authorization

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -92,7 +92,7 @@ authRouter.post('/logout',
             res.send(401)
         } else {
             const expiredTime = await jwtUtility.getExpiredTimeForRefresh(refreshToken)
-            if (expiredTime && Date.now() / 1000 > +expiredTime) {
+            if (!expiredTime || expiredTime && Date.now() / 1000 > +expiredTime) {
                 res.send(401)
             }
         }
